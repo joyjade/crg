540459814ba742736c8badae9f8a7f8e52b55f88
Joy Jade <jadeandpearls@gmail.com>
2020-06-10 23:42:24 -0700

    Content additions; Panel edits for reading pages;

--- site/templates/info.php
+++ site/templates/info.php
@@ -3,6 +3,11 @@
 <main class="dotted">
 	<div class="info triptych">
 		<?= $page-> text() ?>
+		<br/>
+		<br/>
+		Instagram → <a href='<?=$page->social()?>' target="_blank"><?=$page->insta()?></a>
+		<br/>
+		Join the group → <?= Html::email($page->email()) ?>
 	</div>
 </main>
 
