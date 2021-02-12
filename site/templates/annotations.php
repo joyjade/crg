252c87506f6221aa0b092d810711baf20da15803
Joy Jade <jadeandpearls@gmail.com>
2021-02-11 19:37:06 -0800

    add annotations as separate pages;

--- site/templates/annotations.php
+++ site/templates/annotations.php
@@ -2,13 +2,13 @@
 <?php 
   // $annotation = page('annotations')->children()->findBy("reading", "- " . $reading->id());
   // ($annotation) ? $annotation->url() : '#';  
+  // $site->find('readings')->children()->listed()->flip();
 ?>
 
 
 <main class="<?= $page ?>">
 	<ul>
-    <?php $site->find('readings')->children()->listed()->flip() ?>
-    <?php foreach ($page->children()->listed() as $annotation): ?>
+    <?php foreach ($page->children()->listed()->flip() as $annotation): ?>
       <?php $reading = $annotation->reading()->toPage();?>
 			<li class="book-title" data-src="<?= $reading-> image() ? $reading-> image()-> url() : '' ?>">
         <a href="<?= $annotation->url()?>" class="triptych uppercase">
