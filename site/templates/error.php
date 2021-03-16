e80e40f88ae6e4765fd90bf7d2210ee9f6bd8725
Joy Jade <jadeandpearls@gmail.com>
2021-03-16 12:33:13 -0700

    error page;

---
+++ site/templates/error.php
@@ -0,0 +1,7 @@
+<?php snippet('nav') ?>
+<main>
+  <div class="<?= $page ?> triptych-italick">
+    <h2>Sorry, wherever you're trying to go doesn't exist...</h2>
+  </div>
+</main>
+<?php snippet('aside', ['class' => '', 'image' => '']); snippet('footer'); ?>
