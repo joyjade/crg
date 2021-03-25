a26ad89281429ba2494b75cd3817cde277d4cf57
Joy Jade <jadeandpearls@gmail.com>
2021-03-24 19:35:42 -0700

    bookmark details page;

--- site/templates/bookmark.php
+++ site/templates/bookmark.php
@@ -1,17 +1,45 @@
 <?php snippet('nav') ?>
+
+<?php $backurl = $page->parent()->url() ?>
 <main>
   <div class="bookmark triptych">
     <h3 class="header triptych uppercase">
-      <a href="<?=$page->parent()->url()?>">
+      <a href="<?= $backurl ?>">
         <?= $page->parent()-> title() ?>
       </a>
+      <div class="back-button dotted">
+        <h6><a href="<?= $backurl ?>">⟵ Back </a></h6>
+      </div>
     </h3>
     <?php snippet('bookmark_gallery', ['bookmarks' => $page->siblings()->listed()]) ?>
-    <div class="content">
-      <h4 class="uppercase">$6 Donation</h4></br>
-      <?= $page-> details()->kirbytext() ?>
-      <h4 class="uppercase">About the Artist</h4></br>
-      <?= $page-> bio()->kirbytext() ?>
+    <div class="content flex">
+      <div class="w25">
+        <h1>$6</h1>
+        <h4  class="uppercase">Community <br/> Bookmark #1</h4>
+      </div>
+      <div class="w75">
+        <h1>&nbsp</h1>
+        <?= $page-> details()->markdown() ?>
+        <ul class="pay">
+          <li class=" flex">
+            <span>Venmo</span>
+            <span>@Olivia-Leiter</span>
+          </li>
+          <li class="flex">
+            <span>Cash App</span>
+            <span>$CommunityReading</span>
+          </li>
+        </ul>
+        <?= $page->footnotes()->markdown() ?>
+
+      </div>  
+    </div>
+    <div class="content bio flex">
+      <div class="w25"></div>
+      <div class="w75">
+        <h4 class="uppercase">About the Artist</h4></br>
+        <?= $page-> bio()->markdown() ?>
+      </div>
     </div>
   </div>
 </main>
