6d5705d23b2a1cf5b3b22348a2e983350a038bfb
Joy Jade <jadeandpearls@gmail.com>
2021-10-20 12:49:40 -0700

    brief content;

--- site/templates/reviews.php
+++ site/templates/reviews.php
@@ -4,7 +4,7 @@
     <div>
       <h3>
         <a href="<?= $page->url()?>">
-          <span class="uppercase"><?= $page -> title() ?>s&nbsp;
+          <span class="uppercase"><?= $page -> title() ?>&nbsp;
           <?= $page-> subtitle() ?></span>  
         </a>
       </h3>
@@ -13,5 +13,13 @@
       <h3><a href="<?= $page -> find('mast') -> url()?>">About</a></h3>
     </div>
   </div> 
+  <div class="content">
+    <?php foreach($page->children()->listed() as $article):?>
+      <div>
+        <h2><?= $article->title()?> by <?= $article->author()?></h2>
+      </div>
+    <?php endforeach ?>
+
+  </div>
 </main>
 <?php snippet('aside', ['class' => '', 'image' => '']); snippet('footer'); ?>
