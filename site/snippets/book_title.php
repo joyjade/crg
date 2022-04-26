5ba4c37638954a7e01a39b031e3bbcd64c1d7320
JJ <joyjade@JJs-MacBook-Pro-2.local>
2022-04-25 20:35:02 -0700

    temp change on home page for workshop;

--- site/snippets/book_title.php
+++ site/snippets/book_title.php
@@ -1,4 +1,4 @@
 <span class="triptych-italick book-title highlight" 
       data-src="<?=$rdg ->cover() ? $rdg ->cover() -> url() : ''?>">
       <?=$rdg-> title()-> link()?>
-</span> by <?=$rdg->author()?>
+</span>, <?=$rdg->author()?>
